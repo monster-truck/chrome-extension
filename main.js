a4bb97a96bc245eefcbf003e68e73af12bbd5b10
Nate Toll <nate.toll@gmail.com>
2023-02-15 22:00:15 -0500

    main functionality

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 // document.getSelection() -> return selection object;
 // ___.toString() method -> return string of selection from document;
 
+//Set an event listener on the body of the webpage
 
-const id = 56f9a20e34ea4806b01d524f4a64aa28;
-const secret = 768609cb4ec54d698a9f3763c2f7282a;
+/* if this isn't working, change mousup to click */
+//Listens for mouseup
+document.querySelector('body').addEventListener('mouseup', (e) => {
+    //checks if there's a selection
+    //declare variable to store the users selection
+    let highlightedText;
+    //If true, save that selection to our variable
+    if (document.getSelection()) {
+      // parse variable making everything lowercase, and replacing ' ' with '%20'
+      highlightedText = document.getSelection().toString().toLowerCase();
+      highlightedText.replace(/[^a-zA-Z0-9" "]/g, '').replace(/\s/g, '%20').replaceAll('"', '');
+
+      // save variable with youtube search endpoint, and concat search-string to endpoint
+      let youtubeEndpoint = 'https://www.youtube.com/results?search_query='
+      /// it opens a new tab searching youtube with what has been selected
+       window.open(youtubeEndpoint.concat(highlightedText), '_blank')
+      
+    }
+    
+ 
+})
+  
+
+
